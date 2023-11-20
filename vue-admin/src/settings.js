6412a7a60cd670c9c46bd8c897e252404771218d
hy <2993218474@qq.com>
2023-11-20 16:39:37 +0800

    2023.11.20

---
+++ vue-admin/src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // 浏览器tab显示的标题
+  title: '这是标题',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
