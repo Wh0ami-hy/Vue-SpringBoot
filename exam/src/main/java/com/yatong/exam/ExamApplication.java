ea5db8804ca9995fde6a2c2cdc7b5257307ca1d3
_Wh0ami <2993218474@qq.com>
2023-12-02 17:08:24 +0800

    2023.12.02

--- exam/src/main/java/com/yatong/exam/ExamApplication.java
+++ exam/src/main/java/com/yatong/exam/ExamApplication.java
@@ -1,8 +1,10 @@
 package com.yatong.exam;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ExamApplication {
 
